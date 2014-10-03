050a87972a0654fa3ac048c8564a2f2d68fc50c5
Ryan Choi <ryan.mingyu.choi@gmail.com>
2014-10-03 11:27:34 -0700

    Modified name

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div id="footer-container">
 	<div class="footer">
 		<span><img id="phone" src="img/phone.png" />469.258.1214</span>
-		<span id="middle">COPYRIGHT &copy; 2014 RYAN CHOI</span>
+		<span id="middle">COPYRIGHT &copy; 2014 RYAN MINGYU CHOI</span>
 		<span><img id="mail" src="img/mail.png" /><a href="mailto:ryan.mingyu.choi@gmail.com">ryan.mingyu.choi@gmail.com</a></span>
 	</div>
 </div>
